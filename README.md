7c7ee353d331867cf5def2a04e013d49ec09ddfd
MoonstruckOccultist <justgive1234@gmail.com>
2024-01-04 22:58:21 -0700

    finished the tests updated readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SVGLogoMakR
-node application to quickly make a simple (boring) logo
+# SVGLogoMakR 
+
+## Description
+Lets users create a simple logo by choosing the shape & colors
+
+## Table of Contents
+[Installation](#installation) /
+[Contributers](#contributers) /
+
+## Installation
+in a gitbash terminal open to the route folder, run "npm i" and after node has installed the required packages run the command "node index.js"
+
+## Walkthrough Vid
+[Video Link](https://drive.google.com/file/d/1OC3AEhV7iA0n_SOmIcD_74gJ_xS4xXyk/view?usp=sharing)
+
+## Contributers
+[MoonstruckOccultist](https://github.com/MoonstruckOccultist), 
--- test/svg.test.js
+++ test/svg.test.js
@@ -1,14 +1,16 @@
 const questions = require('../shapes/questions');
-const inquirer = require('inquirer');
+const Circle = require('../shapes/circle');
+const Square = require('../shapes/square');
+const Triangle = require('../shapes/triangle');
 
 
 
 
-describe('LogoMakR', () => {
+describe('Questions', () => {
     describe("acronym", () => {
         it('should return false when answer is wrong', () => {
             let input = "oreo";
-            expect(questions[0].validate(input)).toBe(Error);
+            expect(() => questions[0].validate(input)).toThrow('Must be less than 3 chars.');
         });
     });
     describe("textColor", () => {
@@ -17,11 +19,41 @@ describe('LogoMakR', () => {
             expect(questions[1].validate(input)).toBe(true);
         });
     });
-
     describe("shapeColor", () => {
         it('should return true when answer meets requirement', () => {
             let input = "ff0666";
             expect(questions[3].validate(input)).toBe(true);
         });
     });
+});
+
+describe("Shapes", () => {
+    describe("Circle", () => {
+        it('when it runs the render function should be given the expected result', () => {
+            let input = new Circle('ooo', '000000', 'ffffff');
+            expect(input.render()).toEqual(`<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="150" cy="100" r="80" fill="#ffffff" />
+    <text x="150" y="120" font-size="60" text-anchor="middle" fill="#000000">OOO</text>
+</svg>`);
+        });
+    });
+    describe("Square", () => {
+        it('when it runs the render function should be given the expected result', () => {
+            let input = new Square('ooo', '000000', 'ffffff');
+            expect(input.render()).toEqual(`<svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+    <rect x="50" y="50" width="200" height="200" fill="#ffffff" />
+    <text x="125" y="140" font-size="50" text-anchor="middle" fill="#000000">OOO</text>
+</svg>`);
+        });
+    });
+    describe("Triangle", () => {
+        it('when it runs the render function should be given the expected result', () => {
+            let input = new Triangle('ooo', '000000', 'ffffff');
+            expect(input.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <polygon points="150, 18 244, 182 56, 182" fill="#ffffff" />
+    <text x="150" y="140" font-size="40" text-anchor="middle" fill="#000000">OOO</text>
+</svg>`);
+        });
+    });
+
 });
